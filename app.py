6b7d0476107b5d8ea72117d98388739eb67ec998
J0HNDo032 <91147857+J0HNDo032@users.noreply.github.com>
2023-10-23 18:15:19 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def format():
+    with open("details.txt", 'r') as f:
+        data = {}
+        for i in f.readlines():
+            if i in data.keys():
+                data[i] += 1
+            else:
+                data[i] = 1
+    text = ""
+    for i in data:
+        text += f"{i}, {data[i]}\n"
+    return text
+
+
+@app.route("/")
+def index():
+    return format()
+
+
+@app.route("/insert/<username>")
+def insert(username):
+    with open("details.txt", "a") as f:
+        f.write(username)
+    return "success", 200
+
+
+@app.route("/reset/akhilesh/fuckoff")
+def reset():
+    with open("details.txt", "w") as f:
+        f.write("")
+    return "success", 200
+
+
+app.run()
